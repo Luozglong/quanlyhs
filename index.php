d38595272203fea97213691832083eb2108ece58
Luozglong <117190084+Luozglong@users.noreply.github.com>
2024-12-25 10:11:41 +0700

    m

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+echo "<div class = \"conten2\">bai tap lon dien toan dam may azure 4";
+include "indexs2.html";
+$serverName = "banhang3.database.windows.net"; // update me
+$connectionOptions = array(
+    "Database" => "data2", // update me
+    "Uid" => "adminroot", // update me
+    "PWD" => "Dttmcnd3@" // update me
+);
+//Establishes the connection
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+$tsql= "SELECT * FROM [dbo].[vandon] ";
+$getResults= sqlsrv_query($conn, $tsql);
+echo ("Reading data from table</div>" . PHP_EOL);
+if ($getResults == FALSE){
+    echo (sqlsrv_errors());}
+
+while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) { 
+    echo ("<div class=\"box\"><a>".$row['nguoinhan'] . " " . $row['dienthoai']."</a></div>" . PHP_EOL);
+}   
+sqlsrv_free_stmt($getResults);
+echo"</body></html>";
+?>
